32eaee5a5c0922124906d8ec942a56de73aad834
Pooh <sophonjampasornklin@gmail.com>
2022-03-10 00:47:55 +0700

    setup gh-page

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = withImages({
   //     },
   //   ];
   // // },
-  // basePath: "/bkk-election",
+  basePath: "/bkk-election",
   webpack(config, options) {
     return config;
   },
