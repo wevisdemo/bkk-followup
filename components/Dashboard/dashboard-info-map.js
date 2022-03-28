7e606fb1383e3ee21f4bd1292113c40a402f768a
Pooh <sophonjampasornklin@gmail.com>
2022-03-28 21:05:12 +0700

    change size detail point map water

--- components/Dashboard/dashboard-info-map.js
+++ components/Dashboard/dashboard-info-map.js
@@ -1,59 +1,59 @@
-import React from 'react';
-import * as d3 from 'd3';
-import { isMobileOnly, isMobile } from 'react-device-detect';
-import noData from 'assets/images/no_data.svg';
+import React from "react";
+import * as d3 from "d3";
+import { isMobileOnly, isMobile } from "react-device-detect";
+import noData from "assets/images/no_data.svg";
 
 const info_map = ({ selected_theme }) => {
   let infos, dots1, dots2;
-  if (selected_theme.name === 'น้ำท่วมถนน') {
+  if (selected_theme.name === "น้ำท่วมถนน") {
     infos = [
-      { id: 1, des: 'ระดับความสูงของน้ำโดยเฉลี่ย\nมากกว่าพื้นที่อื่น' },
-      { id: 2, des: 'ระดับความสูงของน้ำโดยเฉลี่ย' },
-      { id: 3, des: 'ระดับความสูงของน้ำโดยเฉลี่ย\nต่ำกว่าพื้นที่อื่น' },
+      { id: 1, des: "ระดับความสูงของน้ำโดยเฉลี่ย\nมากกว่าพื้นที่อื่น" },
+      { id: 2, des: "ระดับความสูงของน้ำโดยเฉลี่ย" },
+      { id: 3, des: "ระดับความสูงของน้ำโดยเฉลี่ย\nต่ำกว่าพื้นที่อื่น" },
     ];
     dots1 = [
-      { w: '40px', h: '40px', color: '#1570FF', text: '100 cm +' },
-      { w: '30px', h: '30px', color: '#3A87FF', text: '80 - 100 cm' },
-      { w: '25px', h: '25px', color: '#609EFF', text: '60 - 79 cm' },
-      { w: '20px', h: '20px', color: '#85B5FF', text: '40 - 59 cm' },
+      { w: "20px", h: "20px", color: "#1570FF", text: "100 cm +" },
+      { w: "20px", h: "20px", color: "#3A87FF", text: "80 - 100 cm" },
+      { w: "20px", h: "20px", color: "#609EFF", text: "60 - 79 cm" },
+      { w: "20px", h: "20px", color: "#85B5FF", text: "40 - 59 cm" },
     ];
     dots2 = [
       {
-        w: '15px',
-        h: '15px',
-        color: '#ABCCFF',
-        text: '20 - 39 cm',
+        w: "20px",
+        h: "20px",
+        color: "#ABCCFF",
+        text: "20 - 39 cm",
       },
       {
-        w: '12px',
-        h: '12px',
-        color: '#D0E2FF',
-        text: '0 - 19 cm',
+        w: "20px",
+        h: "20px",
+        color: "#D0E2FF",
+        text: "0 - 19 cm",
       },
     ];
-  } else if (selected_theme.name === 'พื้นที่สีเขียว') {
+  } else if (selected_theme.name === "พื้นที่สีเขียว") {
     infos = [
-      { id: 1, des: 'มีพื้นที่สีเขียวมากกว่ากำหนด' },
-      { id: 2, des: 'ค่ามาตรฐานตามที่ WHO กำหนด\n(9 ตร.ม./คน)' },
-      { id: 3, des: 'มีพื้นที่สีเขียวน้อยกว่ากำหนด' },
+      { id: 1, des: "มีพื้นที่สีเขียวมากกว่ากำหนด" },
+      { id: 2, des: "ค่ามาตรฐานตามที่ WHO กำหนด\n(9 ตร.ม./คน)" },
+      { id: 3, des: "มีพื้นที่สีเขียวน้อยกว่ากำหนด" },
     ];
-  } else if (selected_theme.name === 'มลพิษในคลอง') {
+  } else if (selected_theme.name === "มลพิษในคลอง") {
     infos = [
-      { id: 1, des: 'BOD ในน้ำคลองโดยเฉลี่ย\nเกินมาตรฐาน' },
-      { id: 2, des: 'ค่า BOD มาตรฐาน\n(ไม่เกิน 4 มก./ลิตร)' },
-      { id: 3, des: 'BOD ในน้ำคลองโดยเฉลี่ย\nต่ำกว่ามาตรฐาน' },
+      { id: 1, des: "BOD ในน้ำคลองโดยเฉลี่ย\nเกินมาตรฐาน" },
+      { id: 2, des: "ค่า BOD มาตรฐาน\n(ไม่เกิน 4 มก./ลิตร)" },
+      { id: 3, des: "BOD ในน้ำคลองโดยเฉลี่ย\nต่ำกว่ามาตรฐาน" },
     ];
-  } else if (selected_theme.name === 'ขยะมูลฝอย') {
+  } else if (selected_theme.name === "ขยะมูลฝอย") {
     infos = [
-      { id: 1, des: 'จำนวนขยะต่อคนต่อวัน\nมากกว่าพื้นที่อื่นโดยเฉลี่ย' },
-      { id: 2, des: 'ปริมาณขยะต่อคนต่อวัน\nโดยเฉลี่ยแต่ละปี' },
-      { id: 3, des: 'จำนวนขยะต่อคนต่อวัน\nน้อยกว่าพื้นที่อื่นโดยเฉลี่ย' },
+      { id: 1, des: "จำนวนขยะต่อคนต่อวัน\nมากกว่าพื้นที่อื่นโดยเฉลี่ย" },
+      { id: 2, des: "ปริมาณขยะต่อคนต่อวัน\nโดยเฉลี่ยแต่ละปี" },
+      { id: 3, des: "จำนวนขยะต่อคนต่อวัน\nน้อยกว่าพื้นที่อื่นโดยเฉลี่ย" },
     ];
-  } else if (selected_theme.name === 'ฝุ่นควันเกินมาตรฐาน') {
+  } else if (selected_theme.name === "ฝุ่นควันเกินมาตรฐาน") {
     infos = [
-      { id: 1, des: 'มีค่าฝุ่นมากกว่าที่กำหนด' },
-      { id: 2, des: 'ค่าฝุ่นละออง PM2.5\n(ไม่เกิน 25 มคก./ลบ.ม.)' },
-      { id: 3, des: 'มีค่าฝุ่นน้อยกว่าที่กำหนด' },
+      { id: 1, des: "มีค่าฝุ่นมากกว่าที่กำหนด" },
+      { id: 2, des: "ค่าฝุ่นละออง PM2.5\n(ไม่เกิน 25 มคก./ลบ.ม.)" },
+      { id: 3, des: "มีค่าฝุ่นน้อยกว่าที่กำหนด" },
     ];
   }
   if (isMobileOnly) {
@@ -61,7 +61,7 @@ const info_map = ({ selected_theme }) => {
   }
 
   const new_line = (text) => {
-    return text.split('\n').map((item, key) => {
+    return text.split("\n").map((item, key) => {
       return (
         <span key={key}>
           {item}
@@ -73,74 +73,70 @@ const info_map = ({ selected_theme }) => {
 
   setTimeout(() => {
     if (!isMobile) {
-      d3.selectAll('.circle0')
-        .append('circle')
-        .attr('class', 'info-circle0')
-        .attr('r', 20)
-        .attr('cx', 20)
-        .attr('cy', 20)
-        .style('fill', selected_theme.color);
+      d3.selectAll(".circle0")
+        .append("circle")
+        .attr("class", "info-circle0")
+        .attr("r", 20)
+        .attr("cx", 20)
+        .attr("cy", 20)
+        .style("fill", selected_theme.color);
     }
     if (!isMobile) {
-      d3.selectAll('.circle2')
-        .append('circle')
-        .attr('class', 'info-circle')
-        .attr('r', 5)
-        .attr('cx', 20)
-        .attr('cy', 20)
-        .style('fill', '#FFFFFF');
+      d3.selectAll(".circle2")
+        .append("circle")
+        .attr("class", "info-circle")
+        .attr("r", 5)
+        .attr("cx", 20)
+        .attr("cy", 20)
+        .style("fill", "#FFFFFF");
     }
 
-    d3.selectAll('.circle')
-      .append('circle')
-      .attr('class', 'info-circle')
-      .attr('r', 10)
-      .attr('cx', 20)
-      .attr('cy', 20)
-      .style('fill', 'none')
-      .style('stroke-width', 1)
-      .style('stroke', 'white')
-      .style('stroke-linecap', 'round')
-      .style('stroke-dasharray', '5 4');
+    d3.selectAll(".circle")
+      .append("circle")
+      .attr("class", "info-circle")
+      .attr("r", 10)
+      .attr("cx", 20)
+      .attr("cy", 20)
+      .style("fill", "none")
+      .style("stroke-width", 1)
+      .style("stroke", "white")
+      .style("stroke-linecap", "round")
+      .style("stroke-dasharray", "5 4");
   }, 100);
 
-  d3.select('.info-circle0').style('fill', selected_theme.color);
+  d3.select(".info-circle0").style("fill", selected_theme.color);
 
   return (
     <div
       className="absolute bottom-0 right-0 flex flex-col items-start flex-1 px-10 pb-5 text-white-default lg:relative "
       id="info-map-wrpper"
     >
-      {selected_theme.name != 'น้ำท่วมถนน' ? (
+      {selected_theme.name != "น้ำท่วมถนน" ? (
         <>
           {infos.map((info, index) => (
             <div id="wrapper" key={index} className="flex items-center">
-              <div id="circle-wrpper" style={{ width: '40px', height: '40px' }}>
+              <div id="circle-wrpper" style={{ width: "40px", height: "40px" }}>
                 <svg className={`circle circle${index}`} />
               </div>
               <p className="pl-3 mt-1 leading-4 p2">{new_line(info.des)}</p>
             </div>
           ))}
-          <div className="items-center hidden lg:flex" >
-            <div id="circle-wrpper" className='flex items-center justify-center' style={{ width: '40px', height: '40px' }}>
+          <div className="items-center hidden lg:flex">
+            <div
+              id="circle-wrpper"
+              className="flex items-center justify-center"
+              style={{ width: "40px", height: "40px" }}
+            >
               <img src={noData} alt="noData" />
             </div>
-            <p
-              className="pl-3 mt-1 leading-4 p2"
-            >
-              ไม่มีข้อมูล
-            </p>
+            <p className="pl-3 mt-1 leading-4 p2">ไม่มีข้อมูล</p>
           </div>
         </>
       ) : !isMobile ? (
         <div id="flood-wrapper" className="flex w-full">
           <div className="flex flex-col items-center flex-1" id="dot1">
             {dots1.map((dot, index) => (
-              <div
-                key={index}
-                className="flex w-full"
-                style={{ height: '40px' }}
-              >
+              <div key={index} className="flex w-full" style={{ height: "40px" }}>
                 <div className="flex items-center justify-end flex-1 my-1">
                   <span
                     className="rounded-full dot"
@@ -151,10 +147,7 @@ const info_map = ({ selected_theme }) => {
                     }}
                   />
                 </div>
-                <p
-                  className="flex items-center pl-2 font-bold p2"
-                  style={{ flex: '2' }}
-                >
+                <p className="flex items-center pl-2 font-bold p2" style={{ flex: "2" }}>
                   {dot.text}
                 </p>
               </div>
@@ -162,11 +155,7 @@ const info_map = ({ selected_theme }) => {
           </div>
           <div className="flex flex-col items-center flex-1" id="dot2">
             {dots2.map((dot, index) => (
-              <div
-                key={index}
-                className="flex w-full"
-                style={{ height: '40px' }}
-              >
+              <div key={index} className="flex w-full" style={{ height: "40px" }}>
                 <div className="flex items-center justify-end flex-1 my-1">
                   <span
                     className="rounded-full dot"
@@ -177,68 +166,58 @@ const info_map = ({ selected_theme }) => {
                     }}
                   />
                 </div>
-                <p
-                  className="flex items-center pl-2 font-bold p2"
-                  style={{ flex: '2' }}
-                >
+                <p className="flex items-center pl-2 font-bold p2" style={{ flex: "2" }}>
                   {dot.text}
                 </p>
               </div>
             ))}
-            <div className="flex w-full" style={{ height: '40px' }}>
+            <div className="flex w-full" style={{ height: "40px" }}>
               <div className="flex items-center justify-end flex-1 my-1">
-                <img src={noData} alt="noData" style={{ height: '19px' }} />
+                <img src={noData} alt="noData" style={{ height: "19px" }} />
               </div>
-              <p
-                className="flex items-center pl-2 font-bold p2"
-                style={{ flex: '2' }}
-              >
+              <p className="flex items-center pl-2 font-bold p2" style={{ flex: "2" }}>
                 ไม่มีข้อมูล
               </p>
             </div>
           </div>
         </div>
       ) : (
-        ''
+        ""
       )}
-      {isMobile && selected_theme.name === 'น้ำท่วมถนน' ? (
+      {isMobile && selected_theme.name === "น้ำท่วมถนน" ? (
         <div className="flex items-center p2">
           <div className="mr-2">น้อย</div>
           <div className="flex items-center justify-end flex-1 mx-1">
-            <img
-              src={noData}
-              alt="noData"
-              style={{ height: isMobileOnly ? '15px' : '19px' }}
-            />
+            <img src={noData} alt="noData" style={{ height: isMobileOnly ? "15px" : "19px" }} />
           </div>
           <span
             className="mx-1 rounded-full dot"
             style={{
-              height: isMobileOnly ? '10px' : '15px',
-              width: isMobileOnly ? '10px' : '15px',
-              backgroundColor: '#ABCCFF',
+              height: isMobileOnly ? "10px" : "15px",
+              width: isMobileOnly ? "10px" : "15px",
+              backgroundColor: "#ABCCFF",
             }}
           />
           <span
             className="mx-1 rounded-full dot"
             style={{
-              height: isMobileOnly ? '20px' : '25px',
-              width: isMobileOnly ? '20px' : '25px',
-              backgroundColor: '#609EFF',
+              height: isMobileOnly ? "20px" : "25px",
+              width: isMobileOnly ? "20px" : "25px",
+              backgroundColor: "#609EFF",
             }}
           />
           <span
             className="mx-1 rounded-full dot"
             style={{
-              height: isMobileOnly ? '30px' : '40px',
-              width: isMobileOnly ? '30px' : '40px',
-              backgroundColor: '#1570FF',
+              height: isMobileOnly ? "30px" : "40px",
+              width: isMobileOnly ? "30px" : "40px",
+              backgroundColor: "#1570FF",
             }}
           />
           <div className="ml-2">มาก</div>
         </div>
       ) : (
-        ''
+        ""
       )}
     </div>
   );
--- components/Dashboard/util/AVG.js
+++ components/Dashboard/util/AVG.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import Rating from 'components/Dashboard/util/rating';
-import exclamation from 'assets/images/exclamation.svg';
-import smile from 'assets/images/smile.svg';
+import React from "react";
+import Rating from "components/Dashboard/util/rating";
+import exclamation from "assets/images/exclamation.svg";
+import smile from "assets/images/smile.svg";
 
-const AVG = ({
-  selected_theme,
-  data,
-  checked,
-  state_dropdown,
-  district_data,
-}) => {
+const AVG = ({ selected_theme, data, checked, state_dropdown, district_data }) => {
   let AVG_title,
     unit,
     AVG_max,
@@ -34,63 +28,62 @@ const AVG = ({
   }
   year_length = Object.keys(data.valuePerYear).length;
 
-  if (selected_theme.name === 'น้ำท่วมถนน') {
-    AVG_title = 'ระดับน้ำท่วมบนถนนโดยเฉลี่ยตั้งแต่ปี 2555-2564';
-    unit = 'ซม.';
-    unit_filter = 'ครั้ง';
-    AVG_max = 'เขตที่มีระดับน้ำท่วมบนถนนโดยเฉลี่ยต่ำที่สุด';
-    AVG_min = 'เขตที่มีระดับน้ำท่วมบนถนนโดยเฉลี่ยสูงที่สุด';
+  if (selected_theme.name === "น้ำท่วมถนน") {
+    AVG_title = "ระดับน้ำท่วมบนถนนโดยเฉลี่ยตั้งแต่ปี 2555-2564";
+    unit = "ซม.";
+    unit_filter = "ครั้ง";
+    AVG_max = "เขตที่มีระดับน้ำท่วมบนถนนโดยเฉลี่ยต่ำที่สุด";
+    AVG_min = "เขตที่มีระดับน้ำท่วมบนถนนโดยเฉลี่ยสูงที่สุด";
     AVG = (sum(data.valuePerYear) / year_length).toFixed(2);
-    compare_title = 'เปรียบเทียบจำนวนครั้งทั้งหมดที่น้ำท่วมแต่ละกลุ่มเขต';
-    rank_text = 'ปี 2564 เขตนี้น้ำท่วมบ่อยสุดเป็นอันดับที่';
-  } else if (selected_theme.name === 'พื้นที่สีเขียว') {
-    AVG_title = 'สัดส่วนพื้นที่สีเขียวต่อประชากร 1 คนโดยเฉลี่ย ปี 2564';
-    unit = 'ตร.ม./คน';
-    unit_filter = 'ตร.ม./คน';
-    AVG_max = 'เขตที่มีพื้นที่สีเขียวมากที่สุด';
-    AVG_min = 'เขตที่มีพื้นที่สีเขียวน้อยที่สุด';
+    compare_title = "เปรียบเทียบจำนวนครั้งทั้งหมดที่น้ำท่วมแต่ละกลุ่มเขต";
+    rank_text = "ปี 2564 เขตนี้น้ำท่วมบ่อยสุดเป็นอันดับที่";
+  } else if (selected_theme.name === "พื้นที่สีเขียว") {
+    AVG_title = "สัดส่วนพื้นที่สีเขียวต่อประชากร 1 คนโดยเฉลี่ย ปี 2564";
+    unit = "ตร.ม./คน";
+    unit_filter = "ตร.ม./คน";
+    AVG_max = "เขตที่มีพื้นที่สีเขียวมากที่สุด";
+    AVG_min = "เขตที่มีพื้นที่สีเขียวน้อยที่สุด";
     AVG = data.value;
-    compare_title = 'เปรียบเทียบสัดส่วนขยะมูลฝอย';
-    compare_sub = 'ต่อประชากร 1 คนต่อวันแต่ละกลุ่มเขต';
-    rank_text = 'ปี 2564 เขตนี้มีพื้นที่สีเขียวต่อคน มากที่สุดเป็นอันดับ';
+    compare_title = "เปรียบเทียบสัดส่วนขยะมูลฝอย";
+    compare_sub = "ต่อประชากร 1 คนต่อวันแต่ละกลุ่มเขต";
+    rank_text = "ปี 2564 เขตนี้มีพื้นที่สีเขียวต่อคน มากที่สุดเป็นอันดับ";
     img_top = exclamation;
     img_bot = smile;
-  } else if (selected_theme.name === 'มลพิษในคลอง') {
-    AVG_title = 'ค่า BOD ในน้ำคลองแต่ละเขตโดยเฉลี่ยตั้งแต่ปี 2555-2564';
-    unit = 'มก./ลิตร';
-    unit_filter = 'มก./ลิตร';
-    AVG_max = 'ค่า BOD ในน้ำคลองโดยเฉลี่ยสูงที่สุด';
-    AVG_min = 'ค่า BOD ในน้ำคลองโดยเฉลี่ยต่ำที่สุด';
+  } else if (selected_theme.name === "มลพิษในคลอง") {
+    AVG_title = "ค่า BOD ในน้ำคลองแต่ละเขตโดยเฉลี่ยตั้งแต่ปี 2555-2564";
+    unit = "มก./ลิตร";
+    unit_filter = "มก./ลิตร";
+    AVG_max = "ค่า BOD ในน้ำคลองโดยเฉลี่ยสูงที่สุด";
+    AVG_min = "ค่า BOD ในน้ำคลองโดยเฉลี่ยต่ำที่สุด";
     AVG = (sum(data.valuePerYear) / year_length - 1).toFixed(2);
-    compare_title = 'เปรียบเทียบค่า BOD ในน้ำคลองแต่ละกลุ่มเขต';
-    rank_text = 'ปี 2564 เขตนี้มีค่า BOD มากที่สุดเป็นอันดับที่';
+    compare_title = "เปรียบเทียบค่า BOD ในน้ำคลองแต่ละกลุ่มเขต";
+    rank_text = "ปี 2564 เขตนี้มีค่า BOD มากที่สุดเป็นอันดับที่";
     img_top = smile;
     img_bot = exclamation;
-  } else if (selected_theme.name === 'ขยะมูลฝอย') {
-    AVG_title = 'จำนวนขยะมูลฝอยโดยเฉลี่ย ปี 2564';
-    unit = 'กก./คน/วัน';
-    unit_filter = 'กก./คน/วัน';
-    AVG_max = 'เขตที่ขยะมากที่สุด';
-    AVG_min = 'เขตที่ขยะน้อยที่สุด';
+  } else if (selected_theme.name === "ขยะมูลฝอย") {
+    AVG_title = "จำนวนขยะมูลฝอยโดยเฉลี่ย ปี 2564";
+    unit = "กก./คน/วัน";
+    unit_filter = "กก./คน/วัน";
+    AVG_max = "เขตที่ขยะมากที่สุด";
+    AVG_min = "เขตที่ขยะน้อยที่สุด";
     AVG = data.value;
-    compare_title = 'เปรียบเทียบสัดส่วนขยะมูลฝอย';
-    compare_sub = 'ต่อประชากร 1 คนต่อวันแต่ละกลุ่มเขต';
-    rank_text = 'ปี 2564 เขตนี้มีปริมาณขยะมูลฝอยต่อคนต่อวันมากสุดเป็นอันดับที่';
+    compare_title = "เปรียบเทียบสัดส่วนขยะมูลฝอย";
+    compare_sub = "ต่อประชากร 1 คนต่อวันแต่ละกลุ่มเขต";
+    rank_text = "ปี 2564 เขตนี้มีปริมาณขยะมูลฝอยต่อคนต่อวันมากสุดเป็นอันดับที่";
     img_top = smile;
     img_bot = exclamation;
-  } else if (selected_theme.name === 'ฝุ่นควันเกินมาตรฐาน') {
-    AVG_title = 'ร้อยละของจำนวนครั้งที่ค่าฝุ่นเกินค่ามาตรฐาน';
-    unit = 'มค.ก./ลบ.ม.';
-    unit_filter = 'มค.ก./ลบ.ม.';
-    AVG_max = 'เขตที่มีค่าสูงสุดของ PM2.5 มากที่สุด';
-    AVG_min = 'เขตที่มีค่าสูงสุดของ PM2.5 น้อยที่สุด';
-    AVG = '74%';
-    compare_title = 'เปรียบเทียบปัญหาฝุ่นควันสูงแต่ละกลุ่มเขต';
-    rank_text = 'ปี 2564 เขตนี้มีค่าสูงสุดของ PM2.5 มากที่สุดเป็นอันดับที่';
+  } else if (selected_theme.name === "ฝุ่นควันเกินมาตรฐาน") {
+    AVG_title = "ร้อยละของจำนวนครั้งที่ค่าฝุ่นเกินค่ามาตรฐาน";
+    unit = "มค.ก./ลบ.ม.";
+    unit_filter = "มค.ก./ลบ.ม.";
+    AVG_max = "เขตที่มีค่าสูงสุดของ PM2.5 มากที่สุด";
+    AVG_min = "เขตที่มีค่าสูงสุดของ PM2.5 น้อยที่สุด";
+    AVG = "92.66%";
+    compare_title = "เปรียบเทียบปัญหาฝุ่นควันสูงแต่ละกลุ่มเขต";
+    rank_text = "ปี 2564 เขตนี้มีค่าสูงสุดของ PM2.5 มากที่สุดเป็นอันดับที่";
     img_top = smile;
     img_bot = exclamation;
   }
-
 
   const box_compare = (data, unit, color) => {
     return (
@@ -98,8 +91,8 @@ const AVG = ({
         id="compare-wrapper"
         className="flex flex-row my-1"
         style={{
-          boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.075)',
-          width: '95%',
+          boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.075)",
+          width: "95%",
         }}
       >
         <div
@@ -110,10 +103,7 @@ const AVG = ({
         >
           {data.areaName}
         </div>
-        <div
-          className="flex items-baseline flex-1 pl-2 font-bold"
-          style={{ color: selected_theme.text_color }}
-        >
+        <div className="flex items-baseline flex-1 pl-2 font-bold" style={{ color: selected_theme.text_color }}>
           <div className="pr-1 h4">{data.value}</div>
           <div className="ml-1 p2">{unit}</div>
         </div>
@@ -128,22 +118,14 @@ const AVG = ({
             className="flex justify-center py-1 rounded-t text-white-default p2"
             style={{ backgroundColor: selected_theme.color }}
           >
-            <img
-              src={img_top}
-              alt={`${img_top}`}
-              className="mr-3"
-              style={{ width: '20px' }}
-            />
+            <img src={img_top} alt={`${img_top}`} className="mr-3" style={{ width: "20px" }} />
             {AVG_min}
           </div>
           <div className="px-2 py-3 leading-tight">
             <p className="font-bold h4">
               เขต{data.minimumPoint.districtName} ปี {data.minimumPoint.year}
             </p>
-            <span
-              className="flex flex-row font-bold h4"
-              style={{ color: selected_theme.text_color }}
-            >
+            <span className="flex flex-row font-bold h4" style={{ color: selected_theme.text_color }}>
               <p>{data.minimumPoint.value}</p>
               <p className="pl-2"> {unit}</p>
             </span>
@@ -154,22 +136,14 @@ const AVG = ({
             className="flex justify-center py-1 rounded-t text-white-default p2"
             style={{ backgroundColor: selected_theme.color }}
           >
-            <img
-              src={img_bot}
-              alt={`${img_bot}`}
-              className="mr-3"
-              style={{ width: '20px' }}
-            />
+            <img src={img_bot} alt={`${img_bot}`} className="mr-3" style={{ width: "20px" }} />
             {AVG_max}
           </div>
           <div className="px-2 py-3 leading-tight">
             <p className="font-bold h4">
               เขต{data.maximumPoint.districtName} ปี {data.maximumPoint.year}
             </p>
-            <span
-              className="flex flex-row font-bold h4"
-              style={{ color: selected_theme.text_color }}
-            >
+            <span className="flex flex-row font-bold h4" style={{ color: selected_theme.text_color }}>
               <p>{data.maximumPoint.value}</p>
               <p className="pl-2"> {unit}</p>
             </span>
@@ -181,36 +155,30 @@ const AVG = ({
 
   const checked_choice = (data, unit) => {
     let choose, default_choose;
-    default_choose = _.filter(data.benchmarks, { zoneId: 'all' });
-    if (checked === 'เขตพื้นที่ธุรกิจ') {
-      choose = _.filter(data.benchmarks, { zoneId: 'business' });
-    } else if (checked === 'เขตพื้นที่ท่องเที่ยวและวัฒนธรรม') {
-      choose = _.filter(data.benchmarks, { zoneId: 'tourism-and-cultural' });
-    } else if (checked === 'เขตพื้นที่อยู่อาศัย') {
-      choose = _.filter(data.benchmarks, { zoneId: 'residence' });
-    } else if (checked === 'เขตพื้นที่ชานเมือง') {
-      choose = _.filter(data.benchmarks, { zoneId: 'suburban' });
+    default_choose = _.filter(data.benchmarks, { zoneId: "all" });
+    if (checked === "เขตพื้นที่ธุรกิจ") {
+      choose = _.filter(data.benchmarks, { zoneId: "business" });
+    } else if (checked === "เขตพื้นที่ท่องเที่ยวและวัฒนธรรม") {
+      choose = _.filter(data.benchmarks, { zoneId: "tourism-and-cultural" });
+    } else if (checked === "เขตพื้นที่อยู่อาศัย") {
+      choose = _.filter(data.benchmarks, { zoneId: "residence" });
+    } else if (checked === "เขตพื้นที่ชานเมือง") {
+      choose = _.filter(data.benchmarks, { zoneId: "suburban" });
     }
 
     return (
-      <div
-        className="flex flex-col flex-1 mt-3 md:mt-3"
-        id="AVG-right"
-        style={{ height: 'fit-content' }}
-      >
+      <div className="flex flex-col flex-1 mt-3 md:mt-3" id="AVG-right" style={{ height: "fit-content" }}>
         <div className="flex-1 card_cat_detail">
           <div
             className="flex flex-col justify-center px-5 py-1 text-center rounded-t text-white-default p2"
             style={{ backgroundColor: selected_theme.color }}
           >
             <p>{compare_title}</p>
-            <p>{compare_sub ? compare_sub : ''}</p>
+            <p>{compare_sub ? compare_sub : ""}</p>
           </div>
           <div className="px-2 py-3 leading-tight">
-            {selected_theme.name === 'น้ำท่วมถนน'
-              ? ''
-              : box_compare(...default_choose, unit, selected_theme.color)}
-            {box_compare(...choose, unit, 'black')}
+            {selected_theme.name === "น้ำท่วมถนน" ? "" : box_compare(...default_choose, unit, selected_theme.color)}
+            {box_compare(...choose, unit, "black")}
           </div>
         </div>
       </div>
@@ -226,51 +194,37 @@ const AVG = ({
         className="flex-1 md:mr-3 card_cat_detail"
         id="AVG-left"
         style={{
-          backgroundColor:
-            selected_theme.name === 'ฝุ่นควันเกินมาตรฐาน'
-              ? selected_theme.color50
-              : '',
+          backgroundColor: selected_theme.name === "ฝุ่นควันเกินมาตรฐาน" ? selected_theme.color50 : "",
         }}
       >
         <div
           className="flex justify-center py-1 rounded-t text-white-default p2"
           style={{ backgroundColor: selected_theme.color }}
         >
-          {selected_theme.name === 'ฝุ่นควันเกินมาตรฐาน'
-            ? 'จำนวนครั้งที่ค่าฝุ่นเกินกำหนด'
-            : 'ค่าเฉลี่ย'}
+          {selected_theme.name === "ฝุ่นควันเกินมาตรฐาน" ? "จำนวนครั้งที่ค่าฝุ่นเกินกำหนด" : "ค่าเฉลี่ย"}
         </div>
         <div className="px-2 py-3">
           <p className="font-bold leading-tight h4">{AVG_title}</p>
-          <p
-            style={{ color: selected_theme.text_color }}
-            className="font-bold h2"
-          >
+          <p style={{ color: selected_theme.text_color }} className="font-bold h2">
             {AVG}
           </p>
-          {selected_theme.name === 'ฝุ่นควันเกินมาตรฐาน' ? (
-            <p
-              style={{ color: selected_theme.text_color }}
-              className="mr-32 font-bold p1"
-            >
-              หรือ 345 ครั้ง จากทั้งหมด 469 ครั้งที่ตรวจวัด
+          {selected_theme.name === "ฝุ่นควันเกินมาตรฐาน" ? (
+            <p style={{ color: selected_theme.text_color }} className="mr-32 font-bold p1">
+              หรือ 139 ครั้ง จากทั้งหมด 150 ครั้งจากที่ตรวจวัด
             </p>
           ) : (
-            <p
-              style={{ color: selected_theme.text_color }}
-              className="mr-32 font-bold p1"
-            >
+            <p style={{ color: selected_theme.text_color }} className="mr-32 font-bold p1">
               {unit}
             </p>
           )}
         </div>
       </div>
 
-      {state_dropdown === 'zone'
+      {state_dropdown === "zone"
         ? checked_district(district_data)
-        : checked === 'เขตพื้นที่ทั้งหมด'
-          ? default_choice(data, unit_filter)
-          : checked_choice(data, unit_filter)}
+        : checked === "เขตพื้นที่ทั้งหมด"
+        ? default_choice(data, unit_filter)
+        : checked_choice(data, unit_filter)}
     </div>
   );
 };
