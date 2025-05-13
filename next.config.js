9dd0c784e4d4f816946137a36f0c91f5664d9bed
MheePooh <sophonjampasornklin@gmail.com>
2025-05-13 11:56:52 +0700

    update path

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const withImages = require("next-images");
 const withTM = require("next-transpile-modules")(["@wevisdemo/ui"]);
 module.exports = withTM(
   withImages({
-    basePath: "",
-    assetPrefix: "",
+    basePath: "/bkk-followup",
+    assetPrefix: "/bkk-followup",
     webpack(config, options) {
       return config;
     },
