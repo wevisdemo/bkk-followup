59dcb627dbff9d4e1832a02ca2f397d3a304a9d0
MheePooh <sophonjampasornklin@gmail.com>
2026-06-03 09:07:53 +0700

    update path map

--- components/Dashboard/dashboard-map.js
+++ components/Dashboard/dashboard-map.js
@@ -288,7 +288,7 @@ const map = ({
     let data_per_year = data.byYear;
     let merge_data = [];
     // d3.csv("/map_info.csv").then((map) => {
-    d3.csv("/map_info.csv").then((map) => {
+    d3.csv("/bkk-followup/map_info.csv").then((map) => {
       let selected_data = data_per_year[`25${selected_year}`];
       _.forIn(selected_data, (data) => {
         const d = map.find((m) => m.districtName === data.districtName);
